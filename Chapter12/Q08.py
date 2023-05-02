5663d3c7dfc6102c8bde1f84c9c8f503ff431a48
Cheoljun Park <dizncjfwns@nate.com>
2023-05-02 14:46:16 +0900

    Update Q08.py

--- Chapter12/Q08.py
+++ Chapter12/Q08.py
@@ -16,3 +16,27 @@ for i in result:
     r += i
 
 print(r+str(cnt))
+
+
+################################################################################
+
+# A08.py
+data = input()
+
+result = []
+value = 0
+
+for x in data:
+    if x.isalpha():
+        result.append(x)
+    else:
+        value += int(x)
+
+        
+result.sort()
+
+if value != 0:
+    result.append(str(value))
+
+
+print(''.join(result))
