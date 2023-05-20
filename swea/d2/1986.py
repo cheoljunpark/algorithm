d6c4bacf4d1d50fce6cf5fed8cfced18f78a9d56
Cheoljun Park <dizncjfwns@nate.com>
2023-05-20 16:30:24 +0900

    Create 1986.py

---
+++ swea/d2/1986.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for i in range(1,T+1):
+    n= int(input())
+
+    sum=0
+    for j in range(1,n+1):
+        if j%2 ==1:
+            sum+=j
+        else:
+            sum-=j
+
+    print(f"#{i} {sum}")
