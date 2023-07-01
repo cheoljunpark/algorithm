59470d3432b14de9d7b4a7a86e67a550ad3863b5
Cheoljun Park <dizncjfwns@nate.com>
2023-07-01 10:31:47 +0900

    Create 각도기.py

---
+++ programmers/코딩테스트입문/각도기.py
@@ -0,0 +1,11 @@
+def solution(angle):
+    if angle > 0 and angle < 90:
+        result = 1
+    elif angle == 90:
+        result = 2
+    elif angle > 90 and angle < 180:
+        result = 3
+    elif angle == 180:
+        result = 4
+    
+    return result
