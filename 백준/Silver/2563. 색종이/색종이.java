3d81c5271dcce0128711862ed87449b31249fa27
Cheoljun Park <dizncjfwns@nate.com>
2023-08-12 14:10:37 +0900

    [Silver V] Title: 색종이, Time: 80 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -4,14 +4,16 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /**
+ * 2차 풀이
  * 
  * @author 박철준
- * 
- * 1. 색종이 수(n)을 입력받는다
- * 2. 각 색종이 마다 위치를 입력받는다
- * 3. 100 x 100 크기의 int 배열(map)을 선언하고
- * 4. 각 색종이 위치에서 가로, 세로 크기 10만큼 map의 각 인덱스에 해당하는 값들을 1로 바꾼다
- * 5. map에서 값 1의 개수를 구해서 출력
+ * 1. 색종이의 수(n)을 입력받는다
+ * 2. 100 x 100 크기의 boolean 배열(map)을 만든다
+ * 3. n개의 색종이의 좌측하단 위치를 입력받는다
+ * 4. 각 좌측하단 위치부터 가로 10, 세로 10 크기만큼
+ *    map의 인덱스를 true로 만든다.
+ * 5. 색종이가 붙은 영역의 넓이를 출력하기 위해
+ *    map의 각 인덱스를 돌면서 true인 부분의 개수를 구한다 
  *
  */
 
@@ -21,41 +23,41 @@ public class Main {
 		// 입출력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
 
-		// 색종이 수 (n) 입력받기
+		// 색종이 개수 입력받기
 		int n = Integer.parseInt(br.readLine().trim());
 
-		// 100 x 100 크기의 map 선언
-		int[][] map = new int[100][100];
+		// 100 x 100 크기 boolean 배열(map) 만들기
+		boolean[][] map = new boolean[100][100];
 
-		for (int i = 0; i < n; i++) { // 각 색종이 마다 위치 입력 받기
-			st = new StringTokenizer(br.readLine().trim());
+		for (int i = 0; i < n; i++) { // 색종이 수 만큼
+			// 색종이의 좌측하단 위치 입력받기
+			StringTokenizer st = new StringTokenizer(br.readLine().trim());
+			int x = Integer.parseInt(st.nextToken()); // 색종이의 좌측하단 x좌표
+			int y = Integer.parseInt(st.nextToken()); // 색종이의 좌측하단 y좌표
 
-			// 좌표 x, y 값 입력받기
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
+			// 좌측하단 위치부터 가로10, 세로 10 만큼
+			// map의 인덱스 true로 만들기
+			for (int rowIdx = x; rowIdx < x + 10; rowIdx++) {
+				for (int colIdx = y; colIdx < y + 10; colIdx++) {
+					map[rowIdx][colIdx] = true;
+				}
+			}
 
-			// 좌표 (x, y)에서 가로, 세로 크기 10만큼
-			// map의 각 인덱스에 해당하는 값들을 1로 바꾸기
-			for (int rowIdx = 0; rowIdx < 10; rowIdx++) {
-				for (int colIdx = 0; colIdx < 10; colIdx++) {
-					map[x + rowIdx][y + colIdx] = 1;
+		}
+
+		// map의 각 인덱스를 돌면서 true의 개수 구하기
+		int cnt = 0;
+		for (int rowIdx = 0; rowIdx < map.length; rowIdx++) {
+			for (int colIdx = 0; colIdx < map[rowIdx].length; colIdx++) {
+				if (map[rowIdx][colIdx] == true) {
+					cnt++;
 				}
 			}
 		}
 
-		// map에서 값 1의 개수를 구해서 출력
-		int area = 0;
-		for (int rowIdx = 0; rowIdx < 100; rowIdx++) {
-			for (int colIdx = 0; colIdx < 100; colIdx++) {
-				if (map[rowIdx][colIdx] == 1) {
-					area++;
-				}
-			}
-		}
-
-		sb.append(area).append("\n");
+		// 출력
+		sb.append(cnt).append("\n");
 		System.out.println(sb);
 	}
 
