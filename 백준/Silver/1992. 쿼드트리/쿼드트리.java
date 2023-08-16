ac30aa64c6e97b8e8a16a60c3ad420c24c530b09
Cheoljun Park <dizncjfwns@nate.com>
2023-08-16 12:11:09 +0900

    [Silver I] Title: 쿼드트리, Time: 108 ms, Memory: 13224 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -13,76 +13,62 @@ import java.io.InputStreamReader;
  *    4.1 // 기저조건
  *        만약 합이 0이면
  *          0리턴, 
- *        만약 합이 1이면 
+ *        만약 합이 nxn이면 
  *          1리턴,
  *    4.2 // 진행로직
  *        아니라면, 
  *          4분할  
  * 
  */
+
 public class Main {
 	static int n;
 	static int[][] map;
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
-	
-	
-	// 합이 0(=모두 0이거나) 또는 nxn(=모두 1이거나)이 될 때까지 분할정복
-	// 만약 합이 0이면
-	//   0리턴, 
-	// 만약 합이 1이면 
-	//   1리턴,
-	// 아니라면, 
-	//   4분할
+
 	public static void divideAndConquer(int r, int c, int n) {
 		// 합 구하기
-		int sum=0;
-		for (int rowIdx = r; rowIdx < r+n; rowIdx++) {
-			for (int colIdx = c; colIdx < c+n; colIdx++) {
+		int sum = 0;
+		for (int rowIdx = r; rowIdx < r + n; rowIdx++) {
+			for (int colIdx = c; colIdx < c + n; colIdx++) {
 				sum += map[rowIdx][colIdx];
 			}
 		}
-		
-		if(sum==0) {
-			sb.append(0);
-		}else if(sum==n*n) {
-			sb.append(1);
-		}else {
-			sb.append("(");
-			divideAndConquer(r, c, n/2);
-			divideAndConquer(r, c+n/2, n/2);
-			divideAndConquer(r+n/2, c, n/2);
-			divideAndConquer(r+n/2, c+n/2, n/2);
+
+		// 합이 0(=모두 0이거나) 또는 nxn(=모두 1이거나)이 될 때까지 분할정복
+		if (sum == 0) { // 만약 합이 0이면
+			sb.append(0); // 0리턴,
+		} else if (sum == n * n) { // 만약 합이 nxn이면
+			sb.append(1); // 1리턴,
+		} else { // 아니라면,
+			sb.append("("); // 4분할
+			divideAndConquer(r, c, n / 2);
+			divideAndConquer(r, c + n / 2, n / 2);
+			divideAndConquer(r + n / 2, c, n / 2);
+			divideAndConquer(r + n / 2, c + n / 2, n / 2);
 			sb.append(")");
 		}
 	}
-	
-	public static void main(String[] args) throws IOException{	
+
+	public static void main(String[] args) throws IOException {
 		// 영상의 크기(n) 입력받기
 		n = Integer.parseInt(br.readLine().trim());
-		
+
 		// nxn 크기의 배열 만들기
 		map = new int[n][n];
-		
+
 		// 배열에 0 또는 1 입력받기
 		for (int rowIdx = 0; rowIdx < n; rowIdx++) {
 			String[] str = br.readLine().trim().split("");
 			for (int colIdx = 0; colIdx < n; colIdx++) {
 				map[rowIdx][colIdx] = Integer.parseInt(str[colIdx]);
-			}	
+			}
 		}
-		
-		
-		// 합이 0(=모두 0이거나) 또는 nxn(=모두 1이거나)이 될 때까지 분할정복
-		// 만약 합이 0이면
-		//   0리턴, 
-		// 만약 합이 1이면 
-		//   1리턴,
-		// 아니라면, 
-		//   4분할
-		
-		divideAndConquer(0,0,n);
-		
+
+		// 분할정복해서 구하기
+		divideAndConquer(0, 0, n);
+
 		System.out.println(sb);
 	}
 
