3f83c62bc2b21b1409805bfd0cff7e379dc4ba60
Cheoljun Park <dizncjfwns@nate.com>
2023-08-30 16:39:46 +0900

    [Silver III] Title: N과 M (1), Time: 172 ms, Memory: 20404 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,54 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-import java.util.Scanner;
+/**
+ * 
+ * @author 박철준
+ * 
+ * 1. 정수(numberSize)와 고를 개수 (selectCount)를 입력받는다
+ * 2. 크기 numberSize인 배열(numberList)를 생성
+ * 3. numberList에 1부터 numberSize까지 저장한다
+ * 4. 순열 메소드를 호출할 때 필요한 방문체크배열(visited)와 고른 것을 담을 배열(selectList)를 생성 
+ * 5. 순열 메소드 호출 -> numberSize_P_selectCount를 구한다
+ *
+ */
 
 public class Main {
-	// 순열 메소드
-	// depth가 r만큼 될 때까지 재귀호출
-	// 유도조건:
-	// 1. 배열(arr)의 인덱스를 차례대로 순회한다
-	// 2. arr의 원소가 뽑힌적 없다면(visited[i]가 false)라면
-	// 3. visited[i]를 true로 바꿔주고
-	// 4. 깊이(depth)에 해당하는 순열저장배열(output[depth])에 뽑은원소(arr[i])를 저장
-	// 5. 다시 depth+1한 permutation 메소드 호출
-	// 6. 재귀가 모두 호출 된 뒤, 다음 순열을 위해 visited[i]는 false로 바꿔준다.
-	// 기저조건:
-	// 1. depth가 r일때(r개가 뽑혔을때)
-	// 2. 순열저장배열(output)을 출력
-	static void permutation(int[] arr, int[] output, boolean[] visited, int depth, int n, int r) {
-		if (depth == r) {	// r개 뽑혔다면
-			for (int idx = 0; idx < r; idx++) {
-				System.out.print(output[idx] + " ");
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 입력
+	static StringBuilder sb = new StringBuilder(); // 출력
+	static StringTokenizer st; // 토큰화
+	static int numberSize; // 정수
+	static int[] numberList; // 1 ~ numberSize까지 담을 배열
+	static int selectCount; // 고를 개수
+	static int[] selectList; // 고른 것 담을 배열
+	static boolean[] visited; // 방문 체크
+
+	static void permutation(int selectCountIdx) {
+		// 기저 조건
+		if (selectCountIdx == selectCount) { // selectCount개 만큼 골랐다면
+			for (int selectNumber : selectList) { // selectList에 있는 원소들 append
+				sb.append(selectNumber).append(" ");
 			}
-			System.out.println();
+			sb.append("\n");
 			return;
 		}
 
-		for (int i = 0; i < n; i++) {
-			if (visited[i] != true) {	// 뽑힌적이 없다면
-				visited[i] = true;	// 뽑아주고
-				output[depth] = arr[i];	// 뽑힌 수(arr[i])를 순열저장배열(ouput[depth])에 저장
-				permutation(arr, output, visited, depth + 1, n, r); // depth를 하나 증가해서 재귀 호출
-				visited[i] = false;
+		// 진행 조건
+		for (int idx = 0; idx < numberList.length; idx++) {
+
+			// 방문했다면 스킵
+			if (visited[idx]) {
+				continue;
 			}
+
+			// 빙문하지 않았다면 아래 로직 수행
+			visited[idx] = true; // 방문 처리하고
+			selectList[selectCountIdx] = numberList[idx]; // 원소를 골라 리스트에 넣고
+			permutation(selectCountIdx + 1); // 다음 순열 재귀 호출
+			visited[idx] = false; // 기저 조건 처리 후 방문 처리한것을 다시 풀기
 		}
+
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
+		// 정수(numberSize)와 고를 개수 (selectCount)를 입력받기
+		st = new StringTokenizer(br.readLine().trim());
+		numberSize = Integer.parseInt(st.nextToken());
+		selectCount = Integer.parseInt(st.nextToken());
 
-		int n = sc.nextInt();	// 문제에선 N
-		int r = sc.nextInt();	// 문제에선 M
+		// 크기 numberSize인 배열(numberList) 생성
+		numberList = new int[numberSize];
 
-		int[] arr = new int[n];	// 1부터 n까지의 정수를 담은 배열
-		int[] output = new int[n];	// 순열 저장 배열
-		boolean[] visited = new boolean[n];	// 인덱스에 해당하는 숫자가 사용 중인지 저장하는 배열
-
-		for (int idx = 0; idx < n; idx++) {
-			arr[idx] = idx + 1;	// 배열에 1부터 n까지 차례대로 담는다
+		// numberList에 1부터 numberSize까지 저장한다
+		for (int idx = 0; idx < numberList.length; idx++) {
+			numberList[idx] = idx + 1;
 		}
 
-		permutation(arr, output, visited, 0, n, r);	// 순열 메소드 호출
+		// 방문 체크(visited) 생성
+		visited = new boolean[numberSize];
+
+		// 고른 것을 담을 배열 생성
+		selectList = new int[selectCount];
+
+		// 순열 메소드 호출
+		permutation(0);
+
+		// 정답 출력
+		System.out.println(sb);
 	}
 
 }
