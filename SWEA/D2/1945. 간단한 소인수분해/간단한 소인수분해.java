aeef0097e3ec0e73d6a10cf5024402f0a6e2afbe
Cheoljun Park <dizncjfwns@nate.com>
2023-11-01 22:17:48 +0900

    [D2] Title: 간단한 소인수분해, Time: 101 ms, Memory: 18,376 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
@@ -2,91 +2,55 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * 
- * @author 박철준
- * 
+/*
  * 1. 테스트 케이스를 입력받는다
- * 2. 각 테스트 케이스별로 정수(n)을 입력받는다
- * 3. 2,3,5,7,11의 개수인 5크기의 배열(cnt)을 선언한다
- * 4. n을 각각 2,3,5,7,11로 나머지가 안 생길때까지의 나눈 횟수를 저장한다
- *
+ * 2. 각 테스트 케이스별로 숫자를 입력받는다
+ * 3. 숫자를 소인수분해 했을 때 지수를 출력한다
+ *    3.1 소수 2, 3, 5, 7, 11을 담은 배열(primeNumList)을 생성
+ *    3.2 primeNumList가 나온 횟수를 담을 배열(primeNumCntList) 생성
+ *    3.3 숫자가 1이 될 때까지
+ *    3.4 숫자를 primeNumList의 각 원소로 나눠서 나누어 떨어지면 primeNumCntList의 인덱스 증가
  */
 
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		// 입출력
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		// 테스트 케이스 입력받기
-		int testCase = Integer.parseInt(br.readLine());
-		
-		// 테스트 케이스 마다
-		for (int tc = 0; tc < testCase; tc++) {
-			int n = Integer.parseInt(br.readLine().trim());	// 정수(n) 입력받기
-			int[] cnt = new int[5];	// 2,3,5,7,11 횟수 담을 배열
-			int idx = 0;	// cnt의 인덱스를 순회할 변수
-			
-			// 나머지가 없을때 까지 2로 나누기
-			while (true) {
-				if (n % 2 != 0) {
-					idx++;
-					break;
-				}
-				n /= 2;
-				cnt[idx]++;
-			}
-			
-			// 나머지가 없을때 까지 3로 나누기
-			while (true) {
-				if (n % 3 != 0) {
-					idx++;
-					break;
-				}
-				n /= 3;
-				cnt[idx]++;
-			}
-			
-			// 나머지가 없을때 까지 5로 나누기
-			while (true) {
-				if (n % 5 != 0) {
-					idx++;
-					break;
-				}
-				n /= 5;
-				cnt[idx]++;
-			}
-			
-			// 나머지가 없을때 까지 7로 나누기
-			while (true) {
-				if (n % 7 != 0) {
-					idx++;
-					break;
-				}
-				n /= 7;
-				cnt[idx]++;
-			}
-			
-			// 나머지가 없을때 까지 11로 나누기
-			while (true) {
-				if (n % 11 != 0) {
-					idx++;
-					break;
-				}
-				n /= 11;
-				cnt[idx]++;
-			}
+        // 테스트 케이스 입력
+        int testCase = Integer.parseInt(br.readLine().trim());
 
-			sb.append("#").append(tc + 1);
-			for (int i = 0; i < cnt.length; i++) {
-				sb.append(" ").append(cnt[i]);
-			}
-			sb.append("\n");
-		}
+        // 각 테스트 케이스 별로
+        for (int tc = 1; tc <= testCase; tc++) {
+            // 숫자 입력받기
+            int number = Integer.parseInt(br.readLine().trim());
 
-		System.out.println(sb);
-	}
+            // 소수 2, 3, 5, 7, 11을 담은 배열(primeNumList)을 생성
+            int[] primeNumList = {2, 3, 5, 7, 11};
 
+            // primeNumList가 나온 횟수를 담을 배열(primeNumCntList) 생성
+            int[] primeNumCntList = new int[5];
+
+            // 숫자가 1이 될 때까지
+            // 숫자를 primeNumList의 각 원소로 나눠서 나누어 떨어지면 primeNumCntList의 인덱스 증가
+            while (number != 1) {
+                for (int idx = 0; idx < primeNumList.length; idx++) {
+                    if (number % primeNumList[idx] == 0) {
+                        primeNumCntList[idx]++;
+                        number /= primeNumList[idx];
+                    }
+                }
+            }
+
+            // primeNumCntList append
+            sb.append("#").append(tc).append(" ");
+            for (int idx = 0; idx < primeNumCntList.length; idx++) {
+                sb.append(primeNumCntList[idx]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        // 정답 출력
+        System.out.println(sb);
+    }
 }
