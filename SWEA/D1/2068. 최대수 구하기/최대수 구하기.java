8d73280737736bb24b5d2adec9ebff3e7fa1c869
Cheoljun Park <dizncjfwns@nate.com>
2023-11-02 20:45:29 +0900

    [D1] Title: 최대수 구하기, Time: 103 ms, Memory: 16,880 KB -BaekjoonHub

--- SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -3,56 +3,55 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * 
- * @author 박철준
- * 
+/*
  * 1. 테스트 케이스를 입력받는다
- * 2. 10개의 정수를 입력받아 배열에 담는다
- * 3. 최댓값을 구한다.
+ * 2. 각 테스트 케이스별로 10개의 수를 입력받는다
+ * 3. 10개의 수 중 최댓값을 찾는다
  *
  */
-
 public class Solution {
-	static int testCase;
-	static int[] numList;
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
+    static int max;
 
-	public static void main(String[] args) throws IOException {
+    /**
+     * 리스트에서 최댓값을 찾는 메소드
+     * 
+     * @param numList 10개의 수가 담긴 배열
+     */
+    static void findMaxNum(int[] numList) {
+        for (int num : numList) {
+            max = Math.max(max, num);
+        }
+    }
 
-		// 테스트 케이스 입력받기
-		testCase = Integer.parseInt(br.readLine().trim());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-		for (int tc = 1; tc <= testCase; tc++) { // 각 테스트 케이스 별로
+        // 테스트 케이스를 입력받는다
+        int testCase = Integer.parseInt(br.readLine().trim());
 
-			// 10개의 정수를 입력받을 배열 선언
-			numList = new int[10];
+        // 각 테스트 케이스별로
+        for (int tc = 1; tc <= testCase; tc++) {
 
-			// 10개의 정수 입력받기
-			st = new StringTokenizer(br.readLine().trim());
-			for (int idx = 0; idx < 10; idx++) {
-				numList[idx] = Integer.parseInt(st.nextToken());
-			}
+            // 10개의 수를 담을 배열 생성
+            int[] numList = new int[10];
 
-			// 최댓값 저장 변수 선언
-			int maxVal = Integer.MIN_VALUE;
+            // 10개의 수를 입력받는다
+            st = new StringTokenizer(br.readLine().trim());
+            for (int idx = 0; idx < 10; idx++) {
+                numList[idx] = Integer.parseInt(st.nextToken());
+            }
 
-			// 최댓값 찾기
-			for (int idx = 0; idx < numList.length; idx++) {
-				if (numList[idx] > maxVal) {
-					maxVal = numList[idx];
-				}
-			}
+            // 최댓값 찾기
+            max = Integer.MIN_VALUE;
+            findMaxNum(numList);
 
-			// 최댓값 append
-			sb.append("#").append(tc).append(" ").append(maxVal).append("\n");
+            // 최댓값 append
+            sb.append("#").append(tc).append(" ").append(max).append("\n");
+        }
 
-		}
-
-		// 정답 출력
-		System.out.println(sb);
-	}
-
+        // 정답 출력
+        System.out.println(sb);
+    }
 }
