36c82c4281799807e93a8fd6737cfa5f0f9fa08e
Cheoljun Park <dizncjfwns@nate.com>
2023-11-04 13:13:48 +0900

    [D1] Title: 중간값 찾기, Time: 100 ms, Memory: 18,556 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -4,42 +4,34 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/**
- * 
- * @author 박철준
- * 
- * 1. 입력받는 점수의 개수(gradeCnt)를 입력받는다
- * 2. 크기가 gradeCnt인 배열 gradeList를 생성
- * 3. 점수를 입력받아 gradeList에 저장
- * 4. gradeList를 정렬
- * 5. gradeList[gradeCnt/2]를 출력
- *
+/*
+ * 1. 리스트의 길이(numCnt)를 입력받는다
+ * 2. 리스트(numList)를 입력받는다
+ * 3. numList를 오름차순 정렬한다
+ * 4. numList[numCnt/2]를 출력
  */
-
 public class Solution {
-	static BufferedReader br;
-	static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-	public static void main(String[] args) throws IOException {
-		br = new BufferedReader(new InputStreamReader(System.in));
+        // 리스트의 길이(numCnt)를 입력받는다
+        int numCnt = Integer.parseInt(br.readLine().trim());
 
-		// 입력받는 점수의 개수(gradeCnt)를 입력받는다
-		int gradeCnt = Integer.parseInt(br.readLine().trim());
+        // 리스트(numList) 생성
+        int[] numList = new int[numCnt];
 
-		// 크기가 gradeCnt인 배열 gradeList를 생성
-		int[] gradeList = new int[gradeCnt];
+        // 리스트(numList)를 입력받는다
+        st = new StringTokenizer(br.readLine().trim());
+        for (int idx = 0; idx < numCnt; idx++) {
+            numList[idx] = Integer.parseInt(st.nextToken());
+        }
 
-		// 점수를 입력받아 gradeList에 저장
-		st = new StringTokenizer(br.readLine().trim());
-		for (int idx = 0; idx < gradeList.length; idx++) {
-			gradeList[idx] = Integer.parseInt(st.nextToken());
-		}
+        // numList 오름차순 정렬
+        Arrays.sort(numList);
 
-		// gradeList를 정렬
-		Arrays.sort(gradeList);
-
-		// gradeList[gradeCnt/2]를 출력
-		System.out.println(gradeList[gradeCnt / 2]);
-	}
-
+        // numList[numCnt/2]를 출력
+        System.out.println(numList[numCnt / 2]);
+    }
 }
