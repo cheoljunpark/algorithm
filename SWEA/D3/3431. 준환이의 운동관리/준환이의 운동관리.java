f9e51882ed00b29f81f34c0b8c6317438418ce4a
Cheoljun Park <dkfnak6789@gmail.com>
2023-12-11 18:38:38 +0900

    [D3] Title: 준환이의 운동관리, Time: 119 ms, Memory: 21,452 KB -BaekjoonHub

--- SWEA/D3/3431. 준환이의 운동관리/준환이의 운동관리.java
+++ SWEA/D3/3431. 준환이의 운동관리/준환이의 운동관리.java
@@ -5,13 +5,13 @@ import java.util.StringTokenizer;
 
 /*
  * 1. 테스트 케이스를 입력받는다
- * 2. 각 테스트 케이스별로 최소운동시간(minExerciseTime), 최대운동시간(maxExerciseTime), 운동한시간(exerciseTime)을 입력받는다
+ * 2. 각 테스트 케이스 별로 최소운동시간(minExerciseTime), 최대운동시간(maxExerciseTime), 운동한시간(exerciseTime)을 입력받는다
  * 3. 운동시간에 따른 추가 운동 시간을 계산한다
  *    3.1 exerciseTime > maxExerciseTime이라면 "-1" 출력
- *    3.2 minExerciseTime < exerciseTime < maxExerciseTime이라면 "0" 출력
- *    3.3 exerciseTime < minExerciseTieml이라면 "minExerciseTime - exerciseTime" 출력
- *
+ *    3.2 minExerciseTime <= exerciseTime <= maxExerciseTime이라면 "0" 출력
+ *    3.3 exerciseTime < minExerciseTieml이라면 "(minExerciseTime - exerciseTime)" 출력
  */
+
 public class Solution {
     static int minExerciseTime, maxExerciseTime, exerciseTime;
     static int testCase;
@@ -22,14 +22,14 @@ public class Solution {
     /**
      * 운동 결과를 반환하는 메소드
      *
-     * @return 필요한 양보다 더 많은 운동을 하고 있다면 -1, 적절하게 하고 있다면 0, 아니라면 추가로 운동해야 하는 시간
+     * @return 필요한 양보다 더 많은 운동을 하고 있다면 -1, 적절하게 하고 있다면 0, 부족하다면 추가로 운동해야 하는 시간
      */
     static int getResult() {
         int result = 0;
 
         if (exerciseTime > maxExerciseTime) {
             result = -1;
-        } else if (exerciseTime > minExerciseTime && exerciseTime < maxExerciseTime) {
+        } else if (exerciseTime >= minExerciseTime && exerciseTime <= maxExerciseTime) {
             result = 0;
         } else {
             result = minExerciseTime - exerciseTime;
