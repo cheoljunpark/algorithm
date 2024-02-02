c1b55b6d7cef9a1951537a4226fad13cedfc6815
Cheoljun Park <dkfnak6789@gmail.com>
2024-02-02 21:19:40 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,20 @@
+from collections import deque
+import math
+def solution(progresses, speeds):
+    answer = []
+    li = deque()
+    for i in range(len(speeds)):
+        li.append(math.ceil((100-progresses[i])/speeds[i]))
+    answer = []
+    a = li.popleft()
+    cnt = 1
+    while li:
+        if li[0]<=a:
+            li.popleft()
+            cnt = cnt + 1
+        else:
+            a = li.popleft()
+            answer.append(cnt)
+            cnt = 1
+    answer.append(cnt)
+    return answer
